88ac316b880b985e4944739ebc0efb305f287b7a
samx23 <skalexsong@gmail.com>
2021-04-10 20:17:12 +0700

    Update : add history cards

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import Container from "../component/Container";
+import Row from "../component/Row";
+import Col from "../component/Col";
+import Card from "../component/Card";
 
 function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
-  )
+    <Container className="mt-5">
+      <Row>
+        <Col className="col-md-10 offset-md-1">
+          <h4>History</h4>
+          <Card className="card">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Original URL</th>
+                  <th>Short URL</th>
+                  <th>Alternative URL</th>
+                  <th>QR URL</th>
+                </tr>
+              </thead>
+              <tbody>
+                <td>xx</td>
+                <td>xx</td>
+                <td>xx</td>
+                <td>xx</td>
+              </tbody>
+            </table>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default History
+export default History;
