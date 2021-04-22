8e6ad6591f5801772bacb2dc846a8deae3d2359d
samx23 <skalexsong@gmail.com>
2021-04-22 19:46:24 +0700

    Update : create error component

---
+++ src/component/Error.js
@@ -0,0 +1,18 @@
+import Container from "../component/Container";
+import Row from "../component/Row";
+import Col from "../component/Col";
+
+const Error = (data) => (
+  <Container className="mt-5">
+    <Row>
+      <Col className="col-12 col-md-12">
+        <div className="text-center">
+          <h4>Oops ! Something went wrong !</h4>
+          <p>{data.error}</p>
+        </div>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Error;
