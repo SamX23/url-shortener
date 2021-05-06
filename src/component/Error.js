44832a5cdb4d026ae8750c330e5fb611d03e8508
samx23 <skalexsong@gmail.com>
2021-05-06 14:44:37 +0700

    Update : refactor props

--- src/component/Error.js
+++ src/component/Error.js
@@ -2,13 +2,13 @@ import Container from "../component/Container";
 import Row from "../component/Row";
 import Col from "../component/Col";
 
-const Error = (data) => (
+const Error = (error) => (
   <Container className="mt-5">
     <Row>
       <Col className="col-12 col-md-12">
         <div className="text-center">
           <h4>Oops ! Something went wrong !</h4>
-          <p>{data.error}</p>
+          <p>{error.data}</p>
         </div>
       </Col>
     </Row>
