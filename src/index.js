5bb6ae6864694e37734e6c6b78074a93938795ac
samx23 <skalexsong@gmail.com>
2021-05-06 14:45:35 +0700

    Update : use react context provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { StateProvider } from "./context/StateProvider";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StateProvider>
+    <App />
+  </StateProvider>,
+  document.getElementById("root")
+);
