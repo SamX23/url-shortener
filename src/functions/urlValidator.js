191ce5ff538eed4b063483280617101e73115e05
samx23 <skalexsong@gmail.com>
2021-05-09 15:02:12 +0700

    Create : refactor separate validation

---
+++ src/functions/urlValidator.js
@@ -0,0 +1,14 @@
+const UrlValidation = (link) => {
+  let pattern = new RegExp(
+    "^(https?:\\/\\/)?" +
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(link);
+};
+
+export default UrlValidation;
