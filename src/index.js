346394309bc46c9b901d8e671d9ef580dc4528e4
samx23 <skalexsong@gmail.com>
2021-05-09 15:02:27 +0700

    Update : use context api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { StateProvider } from "./context/StateProvider";
+import reducer, { initialState } from "./context/reducer";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <StateProvider>
+  <StateProvider reducer={reducer} initialState={initialState}>
     <App />
   </StateProvider>,
   document.getElementById("root")
